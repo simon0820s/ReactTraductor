4089a73e3c9542f18e7cce496b2064a23c2b1a9c
simon0820s <simon0820s@gmail.com>
2023-07-12 15:30:07 -0500

    create initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import './App.css'
+
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
 
 function App () {
   return (
-    <div>app</div>
+    <div className='App'>
+      <h1>Google Translate </h1>
+    </div>
   )
 }
 
