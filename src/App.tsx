2960b362cc8ac84c00e4b7aaee086cd8f220e1da
simon0820s <simon0820s@gmail.com>
2023-07-12 21:16:17 -0500

    create initial state and reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import './App.css'
+import { useReducer } from 'react'
+import { type State } from './types.d'
 
-const initialState = {
+const initialState: State = {
   fromLanguage: 'auto',
   toLanguage: 'en',
   fromText: '',
@@ -9,7 +11,52 @@ const initialState = {
   loading: false
 }
 
+function reducer (state: State, action) {
+  const { type, payload } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload
+    }
+  }
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: payload
+    }
+  }
+
+  return state
+}
+
 function App () {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className='App'>
       <h1>Google Translate </h1>
